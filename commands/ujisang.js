4f06e4a66197a88562d58f34097f52e4e3c3e8c8
YiGeon <parkegun@gmail.com>
2022-11-29 16:36:10 +0900

    feat: 유지상 천재

--- commands/ujisang.js
+++ commands/ujisang.js
@@ -7,7 +7,13 @@ module.exports = {
   async execute(interaction) {
     // interaction.user is the object representing the User who ran the command
     // interaction.member is the GuildMember object, which represents the user in the specific guild
-    await interaction.reply('유지상 바보');
+
+    const words = ['바보', '천재', '해삼', '말미잘'];
+
+    // random word from words array
+    const word = words[Math.floor(Math.random() * words.length)];
+
+    await interaction.reply(`유지상은 ${word}입니다.`);
   },
 };
 
