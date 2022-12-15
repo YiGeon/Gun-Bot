9bca35927fd5752bc9435038f14fa2617c282fbb
YiGeon <parkegun@gmail.com>
2022-12-15 11:13:37 +0900

    feat: add word

--- commands/ujisang.js
+++ commands/ujisang.js
@@ -3,12 +3,12 @@ const { SlashCommandBuilder } = require('discord.js');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('유지상')
-    .setDescription('Provides information about the user.'),
+    .setDescription('유지상 바보'),
   async execute(interaction) {
     // interaction.user is the object representing the User who ran the command
     // interaction.member is the GuildMember object, which represents the user in the specific guild
 
-    const words = ['바보', '멍청이', '해삼', '말미잘', '똥꼬', '똥개'];
+    const words = ['천재', '바보', '멍청이', '해삼', '말미잘', '똥개', '응가'];
 
     // random word from words array
     const word = words[Math.floor(Math.random() * words.length)];
