4e348b554637b652eed1a9602c2d01c83dcd9701
YiGeon <parkegun@gmail.com>
2023-08-29 13:19:29 +0900

    fix: 룰렛 머신 수정

--- commands/roulette.js
+++ commands/roulette.js
@@ -5,16 +5,12 @@ module.exports = {
     .setName('룰렛')
     .setDescription('룰렛 돌리기'),
   async execute(interaction) {
-    // interaction.user is the object representing the User who ran the command
-    // interaction.member is the GuildMember object, which represents the user in the specific guild
-
-    // random 1~9
-    const first = Math.floor(Math.random() * 9) + 1;
-    const second = Math.floor(Math.random() * 9) + 1;
-    const third = Math.floor(Math.random() * 9) + 1;
+    const slotItems = ['🍇', '🍉', '🍌', '🍎', '🍒', '🍓', '🍑'];
+    const first = Math.floor(Math.random() * slotItems.length);
+    const second = Math.floor(Math.random() * slotItems.length);
+    const third = Math.floor(Math.random() * slotItems.length);
 
 
     await interaction.reply(`🎰 ${first} | ${second} | ${third} 🎰`);
   },
 };
-
